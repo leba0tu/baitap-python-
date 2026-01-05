3bd839038ee8c5349bb81e98f17844ab8065d4f6
Bao Tu <leba0tu@outlook.com>
2026-01-05 10:26:01 +0700

    Bài 1: Nhập hai số và thực hiện các phép toán cơ bản

---
+++ bai1.py
@@ -0,0 +1,9 @@
+a = int(input("Nhập số a: "))
+b = int(input("Nhập số b: "))
+print("Tổng =", a + b)
+print("Hiệu =", a - b)
+print("Tích =", a * b)
+if b != 0:
+    print("Thương =", a / b)
+else:           
+    print("Không thể chia cho 0")   
